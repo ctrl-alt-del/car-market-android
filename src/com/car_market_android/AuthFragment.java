8edf40767b4021524167bd6ca2d12b6a993aec15
ctrl-alt-del <jiyang881230@gmail.com>
2014-07-24 22:01:03 -0700

    (1) initialize and attach views for edit texts and button; (2) initialize email validator; (3) add implementations for OnClickListener and set it to button;

--- src/com/car_market_android/AuthFragment.java
+++ src/com/car_market_android/AuthFragment.java
@@ -1,15 +1,17 @@
 package com.car_market_android;
 
-import android.app.Activity;
+import org.apache.commons.validator.routines.EmailValidator;
+
 import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 
 public class AuthFragment extends Fragment implements OnClickListener {
 	/**
@@ -17,13 +19,16 @@ public class AuthFragment extends Fragment implements OnClickListener {
 	 * fragment.
 	 */
 	private static final String ARG_SECTION_NUMBER = "section_number";
-	private final Activity activity;
+	private static final EmailValidator EMAIL_VALIDATOR = EmailValidator.getInstance();
+	private EditText Email;
+	private EditText Password;
+	private Button Sign_in;
 
 	/**
 	 * Returns a new instance of this fragment for the given section
 	 * number.
 	 */
-	public static AuthFragment newInstance(Activity act, int sectionNumber) {
+	public static AuthFragment newInstance(int sectionNumber) {
 		AuthFragment fragment = new AuthFragment();
 		Bundle args = new Bundle();
 		args.putInt(ARG_SECTION_NUMBER, sectionNumber);
@@ -31,31 +36,38 @@ public class AuthFragment extends Fragment implements OnClickListener {
 		return fragment;
 	}
 
-	public AuthFragment(Activity act) {
-		this.activity = activity;
-	}
+	public AuthFragment() {}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_auth, container, false);
 
-		EditText email = (EditText) rootView.findViewById(R.id.email);
-		EditText password = (EditText) rootView.findViewById(R.id.password);
+		this.Email = (EditText) rootView.findViewById(R.id.email);
+		this.Password = (EditText) rootView.findViewById(R.id.password);
+		this.Sign_in = (Button) rootView.findViewById(R.id.sign_in);
 
-
+		this.Sign_in.setOnClickListener(this);
 
 		return rootView;
 	}
 
 	@Override
 	public void onClick(View view) {
-		// TODO Auto-generated method stub
 		switch (view.getId()) {
 		case R.id.sign_in:
-			Toast.makeText(this.act, "Sign In button is pressed!", Toast.LENGTH_SHORT).show();
+			
+			String email = this.Email.getText().toString();
+			
+			if (!EMAIL_VALIDATOR.isValid(email)) {
+				Toast.makeText(getActivity(), "Email: " + email + " is not a valid email...", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			
+			Toast.makeText(getActivity(), "Email: "+this.Email.getText()+"\nPassword: "+this.Password.getText(), Toast.LENGTH_SHORT).show();
 			break;
 		default:
+			Toast.makeText(getActivity(), "Unexpected button pressed...", Toast.LENGTH_SHORT).show();
 			break;
 		}
 	}
