f765a434e1aa3db0666ad87642c53447bb0ba340
ctrl-alt-del <jiyang881230@gmail.com>
2014-07-27 10:28:55 -0700

    add documentation for GetRequest class;

--- src/com/car_market_android/AuthFragment.java
+++ src/com/car_market_android/AuthFragment.java
@@ -79,6 +79,20 @@ public class AuthFragment extends Fragment implements OnClickListener {
 			break;
 		}
 	}
+
+	/**
+	 * Method to send a HTTP GET request to the specified url and grab its response
+	 * 
+	 * To access the localhost or 127.0.0.1 of your local server,such as 
+	 * WAMP, MAMP, or LAMP, you need to route through 10.0.2.2 because
+	 * localhost and 127.0.0.1 is the emulator or the android device itself. 
+	 * <br><br>
+	 * For more details, you can visit the official document in 
+	 * <a href="http://developer.android.com/tools/devices/emulator.html#networkaddresses">here</a>
+	 *
+	 * @since 2014-07-26
+	 * @version 1.0
+	 * */
 	private class GetRequest extends AsyncTask<String, Void, HttpResponse> {
 		@Override
 		protected HttpResponse doInBackground(String... params) {
