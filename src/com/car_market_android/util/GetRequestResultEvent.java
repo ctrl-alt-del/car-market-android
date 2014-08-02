a9f0431e75812fd2a06c685c008d8a73795ba76d
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-01 21:38:41 -0700

    add GetRequestResultEvent class;

---
+++ src/com/car_market_android/util/GetRequestResultEvent.java
@@ -0,0 +1,20 @@
+package com.car_market_android.util;
+
+public class GetRequestResultEvent {
+
+	private final int caller;
+	private final String result;
+
+	public GetRequestResultEvent(int caller, String result) {
+		this.caller = caller;
+		this.result = result;
+	}
+
+	public int getCaller() {
+		return this.caller;
+	}
+	
+	public String getResult() {
+		return this.result;
+	}
+}
