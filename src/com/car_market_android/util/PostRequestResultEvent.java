39c1cd293aa5356e09d7a8105230e6d4c4cf4ce1
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-08 20:00:05 -0700

    add PostRequestResultEvent class;

---
+++ src/com/car_market_android/util/PostRequestResultEvent.java
@@ -0,0 +1,20 @@
+package com.car_market_android.util;
+
+public class PostRequestResultEvent {
+
+	private final int caller;
+	private final String result;
+
+	public PostRequestResultEvent(int caller, String result) {
+		this.caller = caller;
+		this.result = result;
+	}
+
+	public int getCaller() {
+		return this.caller;
+	}
+	
+	public String getResult() {
+		return this.result;
+	}
+}
