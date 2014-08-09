d265e99dcfb223ab67d4d07896d10adbba7f33d9
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-08 20:41:39 -0700

    separate GetRequest from AuthFragment to form a standalone class;

--- src/com/car_market_android/AuthFragment.java
+++ src/com/car_market_android/AuthFragment.java
@@ -10,12 +10,12 @@ import org.apache.commons.validator.routines.EmailValidator;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
 import com.car_market_android.util.EventsBus;
+import com.car_market_android.util.GetRequest;
 import com.car_market_android.util.GetRequestResultEvent;
 import com.car_market_android.util.PostRequestResultEvent;
 import com.squareup.otto.Subscribe;
@@ -149,76 +149,7 @@ public class AuthFragment extends Fragment implements OnClickListener {
 		}
 	}
 
-	/**
-	 * Method to send a HTTP GET request to the specified url and grab its response
-	 *
-	 * To access the localhost or 127.0.0.1 of your local server,such as
-	 * WAMP, MAMP, or LAMP, you need to route through 10.0.2.2 because
-	 * localhost and 127.0.0.1 is the emulator or the android device itself.
-	 * <br><br>
-	 * For more details, you can visit the official document in
-	 * <a href="http://developer.android.com/tools/devices/emulator.html#networkaddresses">here</a>
-	 *
-	 * @since 2014-07-26
-	 * @version 1.0
-	 * */
-	private class GetRequest extends AsyncTask<String, Void, GetRequestResultEvent> {
-
-		private final int caller;
-
-		public GetRequest(int caller) {
-			this.caller = caller;
-		}
-
-
-		@Override
-		protected GetRequestResultEvent doInBackground(String... params) {
-			String link = params[0];
-
-			DefaultHttpClient client = new DefaultHttpClient();
-			HttpGet request = new HttpGet(link);
-			request.addHeader("accept", "application/json");
-			request.addHeader("content-type", "application/x-www-form-urlencoded");
-
-			String result = "";
-			try {
-				HttpResponse response = client.execute(request);
-
-				if (response == null) {
-					result = "No Response";
-					return new GetRequestResultEvent(this.caller, result);
-				}
-
-				if (response.getStatusLine().getStatusCode() != 200) {
-					result = "Failed : HTTP error code : " + response.getStatusLine().getStatusCode();
-					return new GetRequestResultEvent(this.caller, result);
-				}
-
-				BufferedReader br = new BufferedReader(
-						new InputStreamReader(response.getEntity().getContent()));
-
-				String line;
-
-				while ((line = br.readLine()) != null) {
-					result += line;
-				}
-
-				return new GetRequestResultEvent(this.caller, result);
-
-			} catch (IOException e) {
-				e.printStackTrace();
-				return new GetRequestResultEvent(this.caller, e.getMessage());
-			} finally {
-				client.getConnectionManager().shutdown();
-			}
-		}
-
-		@Override
-		protected void onPostExecute(GetRequestResultEvent event) {
-			EventsBus.getInstance().post(event);
-		}
-	}
-
+	
 	/**
 	 * Method to send a HTTP POST request to the specified url and grab its response
 	 *
