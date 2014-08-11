4bcb573be1b65a4dd726990c150eb250587260dc
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-11 00:37:15 -0700

    add ApiKey model for gson;

---
+++ src/com/car_market_android/model/ApiKey.java
@@ -0,0 +1,15 @@
+package com.car_market_android.model;
+
+public class ApiKey {
+	
+	private String token;
+
+	public String getToken() {
+		return this.token;
+	}
+
+	public ApiKey setToken(String token) {
+		this.token = token;
+		return this;
+	}
+}
