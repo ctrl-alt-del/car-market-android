04a6f0bdad7cc2e23a768c353a8da6392dff65d4
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-12 00:11:38 -0700

    add user_id field and its getter and setter to ApiKey model;

--- src/com/car_market_android/model/ApiKey.java
+++ src/com/car_market_android/model/ApiKey.java
@@ -3,6 +3,7 @@ package com.car_market_android.model;
 public class ApiKey {
 	
 	private String token;
+	private long user_id;
 
 	public String getToken() {
 		return this.token;
@@ -10,6 +11,15 @@ public class ApiKey {
 
 	public ApiKey setToken(String token) {
 		this.token = token;
+		return this;
+	}
+	
+	public long getUser_id() {
+		return this.user_id;
+	}
+
+	public ApiKey setUser_id(long user_id) {
+		this.user_id = user_id;
 		return this;
 	}
 }
