79b5b9805007711d346b18230e5a47e74694651e
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-13 00:49:40 -0700

    add model of ApiKeyVer;

---
+++ src/com/car_market_android/model/ApiKeyVer.java
@@ -0,0 +1,17 @@
+package com.car_market_android.model;
+
+public class ApiKeyVer {
+	
+	private boolean is_active = false;
+
+	public boolean isIs_active() {
+		return this.is_active;
+	}
+
+	public ApiKeyVer setIs_active(boolean is_active) {
+		this.is_active = is_active;
+		return this;
+	}
+	
+	
+}
