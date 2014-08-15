d6aee089d560b05bba1b8b7f55da4f7dbbf218e4
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-14 19:22:10 -0700

    add Authentication class;

---
+++ src/com/car_market_android/Authentication.java
@@ -0,0 +1,22 @@
+package com.car_market_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class Authentication extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.authentication);
+
+	}
+
+
+	@Override
+	public void onClick(View view) {
+	}
+
+}
