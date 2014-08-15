1f2d1d6b4eb3bed0043681b5fcae3ac3ffe34c58
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-14 20:28:43 -0700

    update AuthFragment to implement Authentication;

--- src/com/car_market_android/AuthFragment.java
+++ src/com/car_market_android/AuthFragment.java
@@ -1,25 +1,17 @@
 package com.car_market_android;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.validator.routines.EmailValidator;
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-
-import com.car_market_android.model.ApiKey;
 import com.car_market_android.model.Vehicle;
 import com.car_market_android.util.EventsBus;
 import com.car_market_android.util.GetRequest;
 import com.car_market_android.util.GetRequestResultEvent;
-import com.car_market_android.util.PostRequest;
 import com.car_market_android.util.PostRequestResultEvent;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.squareup.otto.Subscribe;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -28,7 +20,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -39,13 +30,12 @@ public class AuthFragment extends Fragment implements OnClickListener {
 	 * fragment.
 	 */
 	private static final String ARG_SECTION_NUMBER = "section_number";
-	private static final EmailValidator EMAIL_VALIDATOR = EmailValidator.getInstance();
-	private EditText Email;
-	private EditText Password;
-	private Button Sign_in;
+
+
+	private Button Authentication;
 	private Button Show_Vehicles;
 	private TextView Json_result;
-	
+
 	private SharedPreferences sharedPreferences;
 
 	/**
@@ -65,37 +55,17 @@ public class AuthFragment extends Fragment implements OnClickListener {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		
+
 		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
 		EventsBus.getInstance().register(this);
 
-		if (this.sharedPreferences.contains(getString(R.string.API_TOKEN_KEY))) {
-
-			String token = sharedPreferences.getString(getString(R.string.API_TOKEN_KEY), "");
-
-			// verify the token is still effective in local
-			if (!StringUtils.isBlank(token)) {
-				// verify the token is still effective in remote
-
-				//				if (remote verification pass) {
-				View rootView = inflater.inflate(R.layout.fragment_user, container, false);
-				// register UI items
-				// return rootView;
-				//				}
-			}
-		}
-		
-		
-
 		View rootView = inflater.inflate(R.layout.fragment_auth, container, false);
 
-		this.Email = (EditText) rootView.findViewById(R.id.email);
-		this.Password = (EditText) rootView.findViewById(R.id.password);
-		this.Sign_in = (Button) rootView.findViewById(R.id.sign_in);
+		this.Authentication = (Button) rootView.findViewById(R.id.authentication);
 		this.Show_Vehicles = (Button) rootView.findViewById(R.id.show_vehicles);
 		this.Json_result = (TextView) rootView.findViewById(R.id.json_result);
 
-		this.Sign_in.setOnClickListener(this);
+		this.Authentication.setOnClickListener(this);
 		this.Show_Vehicles.setOnClickListener(this);
 
 		return rootView;
@@ -104,27 +74,11 @@ public class AuthFragment extends Fragment implements OnClickListener {
 	@Override
 	public void onClick(View view) {
 		switch (view.getId()) {
-		case R.id.sign_in:
+		case R.id.authentication:
 
-			String email = this.Email.getText().toString();
-			String password = this.Password.getText().toString();
+			Intent myIntent = new Intent(getActivity(), Authentication.class);
+			startActivity(myIntent);
 
-			if (!EMAIL_VALIDATOR.isValid(email)) {
-				Toast.makeText(getActivity(), "Email: " + email + " is not a valid email...", Toast.LENGTH_SHORT).show();
-				return;
-			}
-			
-			if (email.length() < 6) {
-				Toast.makeText(getActivity(), "password is not valid...", Toast.LENGTH_SHORT).show();
-				return;
-			}
-			
-			List<NameValuePair> contents = new ArrayList<NameValuePair>();
-
-			contents.add(new BasicNameValuePair("user[email]", email));
-			contents.add(new BasicNameValuePair("user[password]", password));
-
-			new PostRequest(R.id.sign_in, contents).execute(getString(R.string.API_ADDRESS) + "/users/signin");
 			break;
 		case R.id.show_vehicles:
 			new GetRequest(R.id.show_vehicles).execute(getString(R.string.API_ADDRESS) + "/vehicles");
@@ -135,56 +89,65 @@ public class AuthFragment extends Fragment implements OnClickListener {
 		}
 	}
 
+
 	@Override
 	public void onDestroy() {
 		EventsBus.getInstance().unregister(this);
 		super.onDestroy();
 	}
 
+
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		if (sharedPreferences != null) {
+			String token = sharedPreferences.getString(getString(R.string.API_TOKEN_KEY), "Oops");
+			this.Json_result.setText(token);
+		}
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+
+		
+
+	}
+
 	@Subscribe
 	public void onGetRequestTaskResult(GetRequestResultEvent event) {
 		switch (event.getCaller()) {
-		case R.id.sign_in:
-			this.Json_result.setText(event.getResult());
-			break;
 		case R.id.show_vehicles:
+
+			((MainActivity) getActivity()).setProfileResult(event.getResult());
+			//			((MainActivity) getActivity()).getActionBar().setSelectedNavigationItem(1);
 			Gson gson = new GsonBuilder().create();
 			Vehicle[] vehicles = gson.fromJson(event.getResult(), Vehicle[].class);
-			
+
 			String msg = "";
 			for (Vehicle vehicle : vehicles) {
-				msg += vehicle.getVin() + " : ";
+				msg += vehicle.getVin() + "\n";
 			}
-			
+
 			this.Json_result.setText(msg);
 			break;
 		default:
-			Toast.makeText(getActivity(), "Unexpected button pressed...", Toast.LENGTH_SHORT).show();
 			break;
 		}
 	}
 
 	@Subscribe
 	public void onPostRequestTaskResult(PostRequestResultEvent event) {
+
+		Gson gson = new GsonBuilder().create();
+
 		switch (event.getCaller()) {
-		case R.id.sign_in:
-
-			Gson gson = new GsonBuilder().create();
-			ApiKey apiKey = gson.fromJson(event.getResult(), ApiKey.class);
-
-			if (this.sharedPreferences.contains(getString(R.string.API_TOKEN_KEY))) {
-				this.Json_result.setText("from ram: " + apiKey.getToken());
-			} else {
-				this.sharedPreferences.edit().putString(getString(R.string.API_TOKEN_KEY), apiKey.getToken()).commit();
-				this.Json_result.setText("from net: " + sharedPreferences.getString(getString(R.string.API_TOKEN_KEY), "Oops"));
-			}
-
-			break;
 		case R.id.show_vehicles:
 			this.Json_result.setText(event.getResult());
 			break;
 		default:
-			Toast.makeText(getActivity(), "Unexpected button pressed...", Toast.LENGTH_SHORT).show();
 			break;
 		}
 	}
