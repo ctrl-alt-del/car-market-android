d674839dd81cbf73e5794e21c39715527a7c56f2
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-19 00:15:19 -0700

    implement registration.xml on Registration;

--- src/com/car_market_android/Registration.java
+++ src/com/car_market_android/Registration.java
@@ -14,9 +14,19 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registration extends Activity implements OnClickListener {
 
+	private EditText Nickname;
+	private EditText Email;
+	private EditText Password;
+	private EditText Password_Confirmation;
+	private Button Terms_and_policy;
+	private Button Sign_up;
+	private Button Cacnel;
 	private SharedPreferences sharedPreferences;
 
 	@Override
@@ -28,6 +38,18 @@ public class Registration extends Activity implements OnClickListener {
 		EventsBus.getInstance().register(this);
 
 		getWindow().setLayout(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+
+		this.Nickname = (EditText) this.findViewById(R.id.nickname_register);
+		this.Email = (EditText) this.findViewById(R.id.email_register);
+		this.Password = (EditText) this.findViewById(R.id.password_register);
+		this.Password_Confirmation = (EditText) this.findViewById(R.id.password_confirmation_register);
+		this.Terms_and_policy = (Button) this.findViewById(R.id.terms_and_policy_register);
+		this.Sign_up = (Button) this.findViewById(R.id.sign_up_register);
+		this.Cacnel = (Button) this.findViewById(R.id.cancel_register);
+
+		this.Terms_and_policy.setOnClickListener(this);
+		this.Sign_up.setOnClickListener(this);
+		this.Cacnel.setOnClickListener(this);
 	}
 
 	@Override
