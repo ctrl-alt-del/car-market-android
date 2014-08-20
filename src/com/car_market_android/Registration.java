5bb672fe93f6a8ff0591fc25f333574ca44a5b48
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-20 00:36:50 -0700

    rename registration.xml to user_create.xml to keep naming convention consistent with remote API routes;

--- src/com/car_market_android/Registration.java
+++ src/com/car_market_android/Registration.java
@@ -43,7 +43,7 @@ public class Registration extends Activity implements OnClickListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.registration);
+		setContentView(R.layout.user_create);
 
 		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 		EventsBus.getInstance().register(this);
