0206ee4075b2c99d667d264fbe2027701b1f98a3
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-26 00:07:02 -0700

    add VehicleAdapterWishlist;

---
+++ src/com/car_market_android/VehicleAdapterWishlist.java
@@ -0,0 +1,169 @@
+package com.car_market_android;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.car_market_android.R;
+import com.car_market_android.model.Vehicle;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+
+public class VehicleAdapterWishlist extends BaseAdapter implements View.OnClickListener{
+
+	Activity activity;
+	List<Vehicle> vehicles;
+	private SharedPreferences sharedPreferences;
+
+	public VehicleAdapterWishlist(Activity activity, List<Vehicle> rows) {
+		this.activity = activity;
+		this.vehicles = rows;
+		this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.activity);
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return vehicles.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return vehicles.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return arg0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+
+		Vehicle vehicle = vehicles.get(position);
+
+		VehicleIndexRowViewHolder holder;
+		if (convertView == null) {
+
+			holder = new VehicleIndexRowViewHolder();
+
+			convertView = View.inflate(this.activity, R.layout.vehicle_index_wishlist_row, null);
+			holder.Title = (TextView) convertView.findViewById(R.id.vehicle_index_wishlist_row_mmy);
+			holder.Vin = (TextView) convertView.findViewById(R.id.vehicle_index_wishlist_row_vin);
+			holder.Delete = (Button) convertView.findViewById(R.id.vehicle_index_wishlist_row_delete);
+
+			convertView.setTag(holder);
+		} else {
+			holder = (VehicleIndexRowViewHolder) convertView.getTag();
+		}
+
+		holder.Title.setText(vehicle.getManufacturer() + ", " + vehicle.getModel() + ", " + vehicle.getYear());
+		holder.Vin.setText(vehicle.getVin());
+
+
+		VehicleIndexRowButtonActionHolder deleteAH = new VehicleIndexRowButtonActionHolder(ButtonAction.LIKE, vehicle);
+
+		holder.Delete.setTag(deleteAH);
+		holder.Delete.setOnClickListener(this);
+
+
+		return convertView;
+	}
+
+	@Override
+	public void onClick(View view) {
+
+		// User reflection to verify the casting is appropriate
+		if (view.getTag() instanceof VehicleIndexRowButtonActionHolder) {
+
+			VehicleIndexRowButtonActionHolder btnActionHolder = (VehicleIndexRowButtonActionHolder) view.getTag();
+
+			Vehicle vehicle = btnActionHolder.getVehicle();
+
+			switch (btnActionHolder.getButtonAction()) {
+			case LIKE:
+				Toast.makeText(this.activity, "Delete is clicked!\n" + vehicle.getVin(), Toast.LENGTH_LONG).show();
+
+				
+//				LinkedList<Vehicle> data = new LinkedList<Vehicle>();
+//				String JSON_DB = sharedPreferences.getString(this.activity.getString(R.string.CM_USER_WISHLIST), "");
+//
+//				Gson gson = new GsonBuilder().create();
+//				
+//				if (!StringUtils.isBlank(JSON_DB)) {
+//					
+//					Vehicle[] vehicles = gson.fromJson(JSON_DB, Vehicle[].class);
+//
+//					for (Vehicle each : vehicles) {
+//						data.add(each);
+//					}
+//				}
+//				
+//				data.add(vehicle);
+//				
+//				JSON_DB = gson.toJson(data.toArray(new Vehicle[]{}));
+//				this.sharedPreferences.edit().putString(this.activity.getString(R.string.CM_USER_WISHLIST), JSON_DB).commit();
+				
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Class to hold the views of vehicle_index_row.xml.
+	 * 
+	 * @since 2014-08-23
+	 * @version 1.0
+	 * */
+	private class VehicleIndexRowViewHolder {
+
+		protected TextView Title;
+		protected TextView Vin;
+		protected Button Delete;
+	}
+
+	/**
+	 * Class to hold vehicle information along with button action, so button
+	 * action can be identified by OnClickListener.
+
+	 * @param buttonAction identifies which button perform the action
+	 * @param vehicle stores the {@link Vehicle} information 
+	 * 
+	 * @since 2014-08-23
+	 * @version 1.0
+	 * */
+	private class VehicleIndexRowButtonActionHolder {
+
+		private final ButtonAction buttonAction;
+		private final Vehicle vehicle;
+
+		public VehicleIndexRowButtonActionHolder(ButtonAction buttonAction, Vehicle vehicle) {
+			this.buttonAction = buttonAction;
+			this.vehicle = vehicle;
+		}
+
+		public ButtonAction getButtonAction() {
+			return this.buttonAction;
+		}
+
+		public Vehicle getVehicle() {
+			return this.vehicle;
+		}
+	}
+}
