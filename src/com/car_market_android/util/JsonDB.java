13acc43b9f34146974becbbfecdd6317bdb5f2f6
ctrl-alt-del <jiyang881230@gmail.com>
2014-08-27 00:17:50 -0700

    add getVehiclesFromJsonDB() method;

--- src/com/car_market_android/util/JsonDB.java
+++ src/com/car_market_android/util/JsonDB.java
@@ -1,5 +1,39 @@
 package com.car_market_android.util;
 
+import java.util.LinkedList;
+
+import org.apache.commons.lang3.StringUtils;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.car_market_android.model.Vehicle;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class JsonDB {
 
+	private static final Gson gson = new GsonBuilder().create();
+	
+	public static LinkedList<Vehicle> getVehiclesFromJsonDB(Activity activity, String JsonDBKey) {
+		
+		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity);
+
+		LinkedList<Vehicle> data = new LinkedList<Vehicle>();
+
+		String JSON_DB = sharedPreferences.getString(JsonDBKey, "");
+
+		if (StringUtils.isBlank(JSON_DB)) {
+			return data;
+		}
+
+		Vehicle[] vehicles = gson.fromJson(JSON_DB, Vehicle[].class);
+
+		for (Vehicle each : vehicles) {
+			data.add(each);
+		}
+
+		return data;
+	}
 }
