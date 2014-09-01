52c6c5db12c130bb84b5b9f4bddb85e59e6befca
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-01 00:11:19 -0700

    implement menu of add.xml and action bar on MyVehicle;

--- src/com/car_market_android/MyVehicle.java
+++ src/com/car_market_android/MyVehicle.java
@@ -2,6 +2,9 @@ package com.car_market_android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MyVehicle extends Activity {
 
@@ -12,4 +15,26 @@ public class MyVehicle extends Activity {
 		setContentView(R.layout.my_vehicles);
 		
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.add, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		switch (item.getItemId()) {
+		case R.id.action_add:
+			Toast.makeText(this, "add button on action bar is clicked...", Toast.LENGTH_LONG).show();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
 }
