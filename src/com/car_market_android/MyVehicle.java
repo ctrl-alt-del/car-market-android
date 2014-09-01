81a1375130d58ea048dd648ba208ec38e2578611
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-01 00:06:27 -0700

    add MyVehicle;

---
+++ src/com/car_market_android/MyVehicle.java
@@ -0,0 +1,15 @@
+package com.car_market_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyVehicle extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.my_vehicles);
+		
+	}
+}
