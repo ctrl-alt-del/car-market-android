39628c3cd45ea311a41cac1dbeb6b5d1bdc8a509
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-07 00:18:24 -0700

    update ApiClient;

--- src/com/car_market_android/ApiClient.java
+++ src/com/car_market_android/ApiClient.java
@@ -1,8 +1,23 @@
 package com.car_market_android;
 
+import android.content.Context;
+import retrofit.RestAdapter;
+
 public class ApiClient {
 
-	public ApiClient() {
-	}
+	private static final String API_ENDPOINT = "http://car-market.herokuapp.com/api/v1";
+	private static CarMarketApiInterface sTwitchTvService;
 
+    public static CarMarketApiInterface getApiClient() {
+    	
+        if (sTwitchTvService == null) {
+            RestAdapter restAdapter = new RestAdapter.Builder()
+                    .setEndpoint(API_ENDPOINT)
+                    .build();
+
+            sTwitchTvService = restAdapter.create(CarMarketApiInterface.class);
+        }
+
+        return sTwitchTvService;
+    }
 }
