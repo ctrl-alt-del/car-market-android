afe9a96743df1f588af77127f2822276dd3c0f1e
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-07 00:12:27 -0700

    update CarMarketApiInterface with getListings() method;

--- src/com/car_market_android/CarMarketApiInterface.java
+++ src/com/car_market_android/CarMarketApiInterface.java
@@ -1,5 +1,16 @@
 package com.car_market_android;
 
+import retrofit.http.GET;
+import retrofit.http.Query;
+import retrofit.Callback;
+
+import java.util.List;
+
+import com.car_market_android.model.Listing;
+
 public interface CarMarketApiInterface {
+	
+	@GET("/listing.json")
+    void getListings(@Query("limit") int limit, @Query("offset") int offset, Callback<List<Listing>> callback);
 
 }
