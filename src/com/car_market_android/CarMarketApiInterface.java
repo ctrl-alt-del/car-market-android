84a03baf2ed4e91da2dd1b401e6740cc74dd92d1
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-08 00:22:19 -0700

    add getVehicles() method to CarMarketApiInterface;

--- src/com/car_market_android/CarMarketApiInterface.java
+++ src/com/car_market_android/CarMarketApiInterface.java
@@ -1,12 +1,15 @@
 package com.car_market_android;
 
 import retrofit.http.GET;
+import retrofit.http.Header;
+import retrofit.http.Path;
 import retrofit.http.Query;
 import retrofit.Callback;
 
 import java.util.List;
 
 import com.car_market_android.model.Listing;
+import com.car_market_android.model.Vehicle;
 
 public interface CarMarketApiInterface {
 	
@@ -22,4 +25,18 @@ public interface CarMarketApiInterface {
 	@GET("/listings.json")
     void getListings(@Query("limit") int limit, @Query("offset") int offset, Callback<List<Listing>> callback);
 
+
+	/**
+	 * Method to get vehicles of a user through API
+	 * 
+	 * @param user_id the user_id
+	 * @param authorization the token with "Token " append in front of it
+	 * @param callback the async call back
+	 * 
+	 * @version 1.0
+	 * */
+	@GET("/users/{user_id}/vehicles.json")
+    void getVehicles(@Path("user_id") long user_id, @Header("authorization") String authorization, Callback<List<Vehicle>> callback);
+
+	
 }
