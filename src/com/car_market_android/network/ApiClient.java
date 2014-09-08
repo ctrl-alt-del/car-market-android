b47ae0ff65934e8e6a67764d9495146cd8c3e822
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-08 00:35:03 -0700

    add isNetworkConnected() method to ApiClient to determine the network status of device;

--- src/com/car_market_android/network/ApiClient.java
+++ src/com/car_market_android/network/ApiClient.java
@@ -1,5 +1,9 @@
 package com.car_market_android.network;
 
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import retrofit.RestAdapter;
 
 public class ApiClient {
@@ -18,5 +22,28 @@ public class ApiClient {
         }
 
         return sTwitchTvService;
+    }
+    
+    /**
+     * Method to check if network is connected
+     * 
+     * @param activity the {@link Activity}
+     * @return true if network is connected
+     * 
+     * @since 2014-09-8
+     * @version 1.0
+     * */
+    private static boolean isNetworkConnected(Activity activity) {
+    	
+    	ConnectivityManager connectivityManager = (ConnectivityManager) activity
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+		NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+		
+		if (activeNetwork == null) {
+			return false;
+		}
+		
+		return activeNetwork.isConnectedOrConnecting();
     }
 }
