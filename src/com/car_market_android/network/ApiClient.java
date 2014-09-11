416847af7553dfed5e2cb436155cd30038da5121
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-11 00:05:50 -0700

    update getApiClient();

--- src/com/car_market_android/network/ApiClient.java
+++ src/com/car_market_android/network/ApiClient.java
@@ -19,8 +19,8 @@ public class ApiClient {
 	 * the end-point of targeting REST API
 	 * */
 	private static final String API_ENDPOINT = "http://car-market.herokuapp.com/api/v1";
-	
-	private static ApiInterface sTwitchTvService;
+
+	private static ApiInterface ApiClient;
 
 	/**
 	 * Method to create or get an running instance to {@link RestAdapter}
@@ -31,50 +31,59 @@ public class ApiClient {
 	 * @since 2014-09-07
 	 * @version 1.0
 	 * */
-    public static ApiInterface getApiClient(Activity activity) {
-    	
-    	/*
-    	 * check if device is connected to network
-    	 * */
+	public static ApiInterface getApiClient(Activity activity) {
+
+		/*
+		 * check if device is connected to network
+		 * */
 		if (!isNetworkConnected(activity)) {
 			Toast.makeText(activity, "Please connect to internet...", Toast.LENGTH_SHORT).show();
 			return null;
 		}
-    	
+
 		/*
 		 * if {@link ApiInterface} has not been created yet, create one
 		 * */
-        if (sTwitchTvService == null) {
-            RestAdapter restAdapter = new RestAdapter.Builder()
-                    .setEndpoint(API_ENDPOINT)
-                    .build();
+		if (ApiClient == null) {
+			RestAdapter restAdapter = new RestAdapter.Builder()
+			.setEndpoint(API_ENDPOINT)
+			.build();
+			
+//			restAdapter.setRequestInterceptor(new RequestInterceptor() {
+//				@Override
+//				public void intercept(RequestFacade request) {
+//					request.addHeader("accept", "application/json");
+//					request.addHeader("content-type", "application/x-www-form-urlencoded");
+//				}
+//			})
 
-            sTwitchTvService = restAdapter.create(ApiInterface.class);
-        }
+			ApiClient = restAdapter.create(ApiInterface.class);
+		}
 
-        return sTwitchTvService;
-    }
-    
-    /**
-     * Method to check if network is connected
-     * 
-     * @param activity the {@link Activity}
-     * @return true if network is connected
-     * 
-     * @since 2014-09-08
-     * @version 1.0
-     * */
-    private static boolean isNetworkConnected(Activity activity) {
-    	
-    	ConnectivityManager connectivityManager = (ConnectivityManager) activity
+		return ApiClient;
+	}
+
+
+	/**
+	 * Method to check if network is connected
+	 * 
+	 * @param activity the {@link Activity}
+	 * @return true if network is connected
+	 * 
+	 * @since 2014-09-08
+	 * @version 1.0
+	 * */
+	private static boolean isNetworkConnected(Activity activity) {
+
+		ConnectivityManager connectivityManager = (ConnectivityManager) activity
 				.getSystemService(Context.CONNECTIVITY_SERVICE);
 
 		NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
-		
+
 		if (activeNetwork == null) {
 			return false;
 		}
-		
+
 		return activeNetwork.isConnectedOrConnecting();
-    }
+	}
 }
