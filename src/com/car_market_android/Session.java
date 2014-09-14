b08e31a9a43c5aad3799a8672ea804ae7910e86c
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-14 00:33:08 -0700

    added constructor and context field to Session;

--- src/com/car_market_android/Session.java
+++ src/com/car_market_android/Session.java
@@ -1,5 +1,17 @@
 package com.car_market_android;
 
+import android.content.Context;
+
 public class Session {
+	
+	private Context mContext;
+	
+	public Session(Context context) {
+        mContext = context;
+    }
+	
+	public CarMarketApplication getCarMarketApplication() {
+		return (CarMarketApplication) mContext;
+	}
 
 }
