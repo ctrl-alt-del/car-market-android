3820e79fc26f87091fa045118017c54216b2f561
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-15 00:15:23 -0700

    added a method for doing job in UI thread synchronously;

--- src/com/car_market_android/network/NetworkUtils.java
+++ src/com/car_market_android/network/NetworkUtils.java
@@ -1,5 +1,26 @@
 package com.car_market_android.network;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 public class NetworkUtils {
+	
+	public static void pauseMainThreadForJob(long duration) {
+		final CountDownLatch latch = new CountDownLatch(1);
+		
+		new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // do something
+                latch.countDown();
+            }
+        }).start();
+		
+        try {
+            latch.await(duration, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+        }
+		
+	}
 
 }
