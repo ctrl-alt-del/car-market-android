0ace5df3eff51c5dc1e42252b9721f8bcf646370
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-15 00:08:12 -0700

    updated CarMarketApplication with Picasso;

--- src/com/car_market_android/CarMarketApplication.java
+++ src/com/car_market_android/CarMarketApplication.java
@@ -1,7 +1,22 @@
 package com.car_market_android;
 
+import com.squareup.picasso.Picasso;
+
 import android.app.Application;
 
 public class CarMarketApplication extends Application {
 
+	private Picasso mPicasso;
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		// TODO: add cache and download client
+		mPicasso = new Picasso.Builder(this).build();
+	}
+
+	public Picasso getPicasso() {
+        return mPicasso;
+    }
 }
