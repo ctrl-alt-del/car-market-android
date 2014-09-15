74f3fa12d5a0f7f98361a161d394643ca5fddc73
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-15 00:17:36 -0700

    added getHeaders() method to centralize all necessary headers;

--- src/com/car_market_android/network/NetworkUtils.java
+++ src/com/car_market_android/network/NetworkUtils.java
@@ -1,10 +1,18 @@
 package com.car_market_android.network;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class NetworkUtils {
 	
+	public static Map<String, String> getHeaders() {
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put("accept", "application/json");
+		headers.put("content-type", "application/x-www-form-urlencoded");
+		return headers;
+	}
 	public static void pauseMainThreadForJob(long duration) {
 		final CountDownLatch latch = new CountDownLatch(1);
 		
