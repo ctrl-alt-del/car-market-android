e9fccf3d19fc21d1df520865e0a61cf5dca1b8ac
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-15 00:06:18 -0700

    updated Session class;

--- src/com/car_market_android/Session.java
+++ src/com/car_market_android/Session.java
@@ -1,14 +1,21 @@
 package com.car_market_android;
 
+import android.app.Activity;
 import android.content.Context;
 
 public class Session {
 	
+	private Activity mActivity;
 	private Context mContext;
 	
-	public Session(Context context) {
-        mContext = context;
+	public Session(Activity activity) {
+		mActivity = activity;
+		mContext = activity.getBaseContext();
     }
+	
+	public Activity getActivity() {
+		return mActivity;
+	}
 	
 	public CarMarketApplication getCarMarketApplication() {
 		return (CarMarketApplication) mContext;
