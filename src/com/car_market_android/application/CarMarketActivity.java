cb8a5925218e2458c5d103df7a9ec50ebf942a01
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-19 00:08:46 -0700

    updated CarMarketActivity class;

--- src/com/car_market_android/application/CarMarketActivity.java
+++ src/com/car_market_android/application/CarMarketActivity.java
@@ -3,5 +3,12 @@ package com.car_market_android.application;
 import android.app.Activity;
 
 public class CarMarketActivity extends Activity {
-
+	
+	public CarMarketApplication getCarMarketApplication() {
+		return (CarMarketApplication) this.getApplication();
+	}
+	
+	public Session getSession() {
+		return getCarMarketApplication().getSession();
+	}
 }
