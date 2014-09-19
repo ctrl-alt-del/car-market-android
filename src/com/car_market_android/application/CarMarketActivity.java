6e04be9276dd5586e0050e6787723efec292f14f
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-19 00:18:20 -0700

    updated onCreate() of CarMarketActivity class to initialize new session;

--- src/com/car_market_android/application/CarMarketActivity.java
+++ src/com/car_market_android/application/CarMarketActivity.java
@@ -1,9 +1,21 @@
 package com.car_market_android.application;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class CarMarketActivity extends Activity {
 	
+	protected Session mSession;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		mSession = new Session(this);
+		getCarMarketApplication().setSession(mSession);
+		
+	}
+
 	public CarMarketApplication getCarMarketApplication() {
 		return (CarMarketApplication) this.getApplication();
 	}
