1e7ff3d9fd2b5b3459ae11326090d4a5c545c315
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-20 00:12:01 -0700

    updated Utilities class with show and dismiss progress dialog methods;

--- src/com/car_market_android/Utilities.java
+++ src/com/car_market_android/Utilities.java
@@ -1,5 +1,22 @@
 package com.car_market_android;
 
-public class Utilities {
+import android.app.ProgressDialog;
+import android.content.Context;
 
+public class Utilities {
+	
+	public static void showProgressDialog(Context context, ProgressDialog progressDialog, int resourceId) {
+		String message = context.getString(resourceId);
+		if (progressDialog == null) {
+			progressDialog = ProgressDialog.show(context, "", message);
+		} else {
+			progressDialog.setMessage(message);
+		}
+	}
+
+	protected static void dismissProgressDialog(ProgressDialog progressDialog) {
+		if (progressDialog != null && progressDialog.isShowing()) {
+			progressDialog.dismiss();
+		}
+	}
 }
