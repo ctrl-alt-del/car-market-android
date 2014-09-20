d5a4bb63e14ad8d4ac11681e6083f625c1d56991
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-20 00:18:09 -0700

    added getCarMarketApplication() method;

--- src/com/car_market_android/Utilities.java
+++ src/com/car_market_android/Utilities.java
@@ -1,9 +1,16 @@
 package com.car_market_android;
 
+import com.car_market_android.application.CarMarketApplication;
+
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 
 public class Utilities {
+	
+	public static CarMarketApplication getCarMarketApplication(Activity activity) {
+		return (CarMarketApplication) activity.getApplication();
+	}
 	
 	public static void showProgressDialog(Context context, ProgressDialog progressDialog, int resourceId) {
 		String message = context.getString(resourceId);
