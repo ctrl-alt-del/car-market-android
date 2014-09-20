36731e9ffd7851230433013682119fa6fa3b0a8b
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-20 00:02:27 -0700

    added show and dismiss methods for progress dialog in CarMarketActivity class;

--- src/com/car_market_android/application/CarMarketActivity.java
+++ src/com/car_market_android/application/CarMarketActivity.java
@@ -1,12 +1,14 @@
 package com.car_market_android.application;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 public class CarMarketActivity extends Activity {
-	
+
 	protected Session mSession;
-	
+	protected ProgressDialog mProgressDialog;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -15,8 +17,24 @@ public class CarMarketActivity extends Activity {
 	public CarMarketApplication getCarMarketApplication() {
 		return (CarMarketApplication) this.getApplication();
 	}
-	
+
 	public Session getSession() {
 		return getCarMarketApplication().getSession();
 	}
+
+	public void showProgressDialog(int resourceId) {
+		String message = getString(resourceId);
+		if (mProgressDialog == null) {
+			mProgressDialog = ProgressDialog.show(this, "", message);
+		} else {
+			mProgressDialog.setMessage(message);
+		}
+	}
+
+	public void dismissProgressDialog() {
+		if (mProgressDialog != null && mProgressDialog.isShowing()) {
+			mProgressDialog.dismiss();
+		}
+	}
+
 }
