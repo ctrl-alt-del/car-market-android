03d9b9dd15fb45dc62c4a4f716a32a3267e4c9ac
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-21 00:05:09 -0700

    updated LogUtils class;

--- src/com/car_market_android/util/LogUtils.java
+++ src/com/car_market_android/util/LogUtils.java
@@ -4,6 +4,18 @@ import android.util.Log;
 
 public class LogUtils {
 
+    public static final String LOG_TAG = "CarMarket";
 
+    public static void debug(String message) {
+        if (message != null) {
+            Log.d(LOG_TAG, message);
+        }
+    }
+
+    public static void error(String message) {
+        if (message != null) {
+            Log.e(LOG_TAG, message);
+        }
+    }
 }
 
