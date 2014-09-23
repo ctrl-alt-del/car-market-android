d6b41b45a988665e914a55fb7f1befe2ceb62f8e
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-23 00:00:35 -0700

    added showSoftKeyboard() method;

--- src/com/car_market_android/Utilities.java
+++ src/com/car_market_android/Utilities.java
@@ -5,6 +5,7 @@ import com.car_market_android.application.CarMarketApplication;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 public class Utilities {
@@ -38,5 +39,18 @@ public class Utilities {
         ((InputMethodManager) systemService).hideSoftInputFromWindow(
                 activity.getCurrentFocus().getWindowToken(),
                 InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+    public static void showSoftKeyboard(Activity activity) {
+        if (activity == null) {
+            return;
+        }
+
+        Object systemService = activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        View currentFocus = activity.getCurrentFocus();
+        if (currentFocus != null) {
+            ((InputMethodManager) systemService).showSoftInput(currentFocus, InputMethodManager.SHOW_IMPLICIT);
+        }
     }
 }
