369f367e65acd9232beafe1a7c4fa2c4029c98d1
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-24 00:06:29 -0700

    added showToast() method;

--- src/com/car_market_android/util/MessageUtils.java
+++ src/com/car_market_android/util/MessageUtils.java
@@ -1,5 +1,12 @@
 package com.car_market_android.util;
 
 
+import android.content.Context;
+import android.widget.Toast;
+
 public class MessageUtils {
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
 }
