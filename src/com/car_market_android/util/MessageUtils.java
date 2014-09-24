c3c04b2e454a4a82ad997db386401d44f75b2151
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-24 00:09:59 -0700

    updated showToastShort() and showToastLong() methods;

--- src/com/car_market_android/util/MessageUtils.java
+++ src/com/car_market_android/util/MessageUtils.java
@@ -6,6 +6,14 @@ import android.widget.Toast;
 
 public class MessageUtils {
 
+    public static void showToastShort(Context context, int resourceId) {
+        showToastShort(context, context.getString(resourceId));
+    }
+
+    public static void showToastLong(Context context, int resourceId) {
+        showToastLong(context, context.getString(resourceId));
+    }
+
     public static void showToastShort(Context context, String message) {
         showToast(context, message, Toast.LENGTH_SHORT);
     }
