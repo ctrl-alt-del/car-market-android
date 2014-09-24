c61cb124e434633b844291e2378ba163cd9a3f53
ctrl-alt-del <jiyang881230@gmail.com>
2014-09-24 00:19:10 -0700

    resolved errors caused by method updates;

--- src/com/car_market_android/Profile_Fragment.java
+++ src/com/car_market_android/Profile_Fragment.java
@@ -11,7 +11,6 @@ import com.car_market_android.network.CarMarketClient;
 import com.car_market_android.util.EventsBus;
 import com.car_market_android.util.MessageUtils;
 
-import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -30,164 +29,6 @@ import android.widget.Toast;
 
 
 public class Profile_Fragment extends CarMarketFragment implements OnClickListener {
-	/**
-	 * Returns a new instance of this fragment for the given section
-	 * number.
-	 */
-	public static Profile_Fragment newInstance(int sectionNumber) {
-		Profile_Fragment fragment = new Profile_Fragment();
-		Bundle args = new Bundle();
-		args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-		fragment.setArguments(args);
-		return fragment;
-	}
-
-	public Profile_Fragment() {}
-
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-
-		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-		EventsBus.getInstance().register(this);
-
-		View rootView = inflater.inflate(R.layout.profile_fragment, container, false);
-
-
-		return rootView;
-	}
-
-	private ProgressDialog dialog;
-	@Override
-	public void onClick(View view) {
-		switch (view.getId()) {
-		case R.id.authentication:
-
-			if (sharedPreferences.contains(getString(R.string.CM_API_TOKEN))) {
-				this.sharedPreferences.edit().remove(getString(R.string.CM_API_TOKEN)).commit();
-				this.sharedPreferences.edit().remove(getString(R.string.CM_USER_NICKNAME)).commit();
-				this.sharedPreferences.edit().remove(getString(R.string.CM_USER_EMAIL)).commit();
-
-				this.setGusetView();
-			} else {
-				Intent authentication_intent = new Intent(getActivity(), UserAuth.class);
-				startActivity(authentication_intent);
-			}
-			break;
-		case R.id.user_update:
-			Intent user_update_intent = new Intent(getActivity(), UserUpdate.class);
-			startActivity(user_update_intent);
-			break;
-		case R.id.registration:
-			Intent registration_intent = new Intent(getActivity(), UserCreate.class);
-			startActivity(registration_intent);
-			break;
-		case R.id.my_vehicles:
-			Intent myvehicle_intent = new Intent(getActivity(), MyVehicle.class);
-			startActivity(myvehicle_intent);
-			break;
-		default:
-			Toast.makeText(getActivity(), "Unexpected button pressed...", Toast.LENGTH_SHORT).show();
-			break;
-		}
-	}
-
-	@Override
-	public void onDestroy() {
-		EventsBus.getInstance().unregister(this);
-		super.onDestroy();
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-		if (sharedPreferences != null) {
-
-			String token = sharedPreferences.getString(getString(R.string.CM_API_TOKEN), "");
-			long user_id = sharedPreferences.getLong(getString(R.string.CM_API_USER_ID), -1);
-
-			if (StringUtils.isBlank(token) || user_id == -1) {
-				this.setGusetView();
-				return;
-			}
-
-			String nickname = sharedPreferences.getString(getString(R.string.CM_USER_NICKNAME), "");
-			String email = sharedPreferences.getString(getString(R.string.CM_USER_EMAIL), "");
-
-			if (!StringUtils.isBlank(nickname) && !StringUtils.isBlank(email)) {
-
-				this.Nickname_profile.setText(nickname);
-				this.Email_profile.setText(email);
-
-				this.setUserView();
-
-				return;
-			}
-
-
-			this.dialog = new ProgressDialog(getActivity());
-			this.dialog.setMessage("Loading Profile...");
-			this.dialog.show();
-
-			CarMarketClient.getInstance(getActivity()).getUser(user_id, "Token " + token, new Callback<User>() {
-
-				@Override
-				public void success(User user, Response response) {
-
-					Nickname_profile.setText(user.getNickname());
-					Email_profile.setText(user.getEmail());
-
-					sharedPreferences.edit().putString(getString(R.string.CM_USER_NICKNAME), user.getNickname()).commit();
-					sharedPreferences.edit().putString(getString(R.string.CM_USER_EMAIL), user.getEmail()).commit();
-
-					setUserView();
-
-					if (dialog.isShowing()) {
-						dialog.dismiss();
-					}
-				}
-
-				@Override
-				public void failure(RetrofitError retrofitError) {
-					/**
-					 * This message is for debug mode.
-					 * */
-					Toast.makeText(getActivity(), retrofitError.getMessage(), Toast.LENGTH_SHORT).show();
-					/**
-					 * This message is for production mode.
-					 * */
-					Toast.makeText(getActivity(), "Connection failed, please try again :(", Toast.LENGTH_SHORT).show();
-
-					if (dialog.isShowing()) {
-						dialog.dismiss();
-					}
-				}
-
-			});
-
-			return;
-		}
-	}
-
-	private void setUserView() {
-		this.Authentication.setText("Sign Out");
-		LayoutParams params = this.User_layout.getLayoutParams();
-		params.height = LayoutParams.WRAP_CONTENT;
-		this.User_layout.setLayoutParams(params);
-
-		this.Registration.setVisibility(View.INVISIBLE);
-		this.Registration.setEnabled(false);
-	}
-
-	private void setGusetView() {
-		this.Authentication.setText("Sign In");
-		LayoutParams params = this.User_layout.getLayoutParams();
-		params.height = 0;
-		this.User_layout.setLayoutParams(params);
-
-		this.Registration.setVisibility(View.VISIBLE);
-		this.Registration.setEnabled(true);
-	}
     /**
      * The fragment argument representing the section number for this
      * fragment.
@@ -212,6 +53,21 @@ public class Profile_Fragment extends CarMarketFragment implements OnClickListen
 
     private SharedPreferences sharedPreferences;
     private ProgressDialog dialog;
+
+    public Profile_Fragment() {
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static Profile_Fragment newInstance(int sectionNumber) {
+        Profile_Fragment fragment = new Profile_Fragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -254,6 +110,137 @@ public class Profile_Fragment extends CarMarketFragment implements OnClickListen
             this.setGusetView();
         }
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+//		case R.id.authentication:
+//
+//			if (sharedPreferences.contains(getString(R.string.CM_API_TOKEN))) {
+//				this.sharedPreferences.edit().remove(getString(R.string.CM_API_TOKEN)).commit();
+//				this.sharedPreferences.edit().remove(getString(R.string.CM_USER_NICKNAME)).commit();
+//				this.sharedPreferences.edit().remove(getString(R.string.CM_USER_EMAIL)).commit();
+//
+//				this.setGusetView();
+//			} else {
+//				Intent authentication_intent = new Intent(getActivity(), UserAuth.class);
+//				startActivity(authentication_intent);
+//			}
+//			break;
+            case R.id.user_update:
+                Intent user_update_intent = new Intent(getActivity(), UserUpdate.class);
+                startActivity(user_update_intent);
+                break;
+            case R.id.sign_up_or_sign_in_button:
+                Intent registration_intent = new Intent(getActivity(), UserCreate.class);
+                startActivity(registration_intent);
+                break;
+            case R.id.my_vehicles:
+                Intent myvehicle_intent = new Intent(getActivity(), MyVehicle.class);
+                startActivity(myvehicle_intent);
+                break;
+            default:
+                Toast.makeText(getActivity(), "Unexpected button pressed...", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        EventsBus.getInstance().unregister(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (sharedPreferences != null) {
+
+            String token = sharedPreferences.getString(getString(R.string.CM_API_TOKEN), "");
+            long user_id = sharedPreferences.getLong(getString(R.string.CM_API_USER_ID), -1);
+
+            if (StringUtils.isBlank(token) || user_id == -1) {
+                this.setGusetView();
+                return;
+            }
+
+            String nickname = sharedPreferences.getString(getString(R.string.CM_USER_NICKNAME), "");
+            String email = sharedPreferences.getString(getString(R.string.CM_USER_EMAIL), "");
+
+            if (!StringUtils.isBlank(nickname) && !StringUtils.isBlank(email)) {
+
+                this.Nickname_profile.setText(nickname);
+                this.Email_profile.setText(email);
+
+                this.setUserView();
+
+                return;
+            }
+
+
+            this.dialog = new ProgressDialog(getActivity());
+            this.dialog.setMessage("Loading Profile...");
+            this.dialog.show();
+
+            CarMarketClient.getInstance(getActivity()).getUser(user_id, "Token " + token, new Callback<User>() {
+
+                @Override
+                public void success(User user, Response response) {
+
+                    Nickname_profile.setText(user.getNickname());
+                    Email_profile.setText(user.getEmail());
+
+                    sharedPreferences.edit().putString(getString(R.string.CM_USER_NICKNAME), user.getNickname()).commit();
+                    sharedPreferences.edit().putString(getString(R.string.CM_USER_EMAIL), user.getEmail()).commit();
+
+                    setUserView();
+
+                    if (dialog.isShowing()) {
+                        dialog.dismiss();
+                    }
+                }
+
+                @Override
+                public void failure(RetrofitError retrofitError) {
+                    /**
+                     * This message is for debug mode.
+                     * */
+                    Toast.makeText(getActivity(), retrofitError.getMessage(), Toast.LENGTH_SHORT).show();
+                    /**
+                     * This message is for production mode.
+                     * */
+                    Toast.makeText(getActivity(), "Connection failed, please try again :(", Toast.LENGTH_SHORT).show();
+
+                    if (dialog.isShowing()) {
+                        dialog.dismiss();
+                    }
+                }
+
+            });
+
+            return;
+        }
+    }
+
+    private void setUserView() {
+//		this.Authentication.setText("Sign Out");
+        LayoutParams params = this.User_layout.getLayoutParams();
+        params.height = LayoutParams.WRAP_CONTENT;
+        this.User_layout.setLayoutParams(params);
+
+        this.mSignUpOrSignInButton.setVisibility(View.INVISIBLE);
+        this.mSignUpOrSignInButton.setEnabled(false);
+    }
+
+    private void setGusetView() {
+//		this.Authentication.setText("Sign In");
+        LayoutParams params = this.User_layout.getLayoutParams();
+        params.height = 0;
+        this.User_layout.setLayoutParams(params);
+
+        this.mSignUpOrSignInButton.setVisibility(View.VISIBLE);
+        this.mSignUpOrSignInButton.setEnabled(true);
     }
     private void setRegistrationAction() {
         MessageUtils.showToastShort(getContext(), "setRegistrationAction() called");
